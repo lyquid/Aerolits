e9629a04e8f40c2dae47019e202b3bfa04232d31
Alex CB <ketepincho@gmail.com>
2021-08-05 17:57:47 +0200

    Improved image wrapper

--- sdl2_wrappers/sdl2_image.hpp
+++ sdl2_wrappers/sdl2_image.hpp
@@ -1,21 +1,48 @@
 #ifndef KTP_SDL2_WRAPPERS_SDL2_IMAGE_HPP_
 #define KTP_SDL2_WRAPPERS_SDL2_IMAGE_HPP_
 
+#include "sdl2_log.hpp"
 #include <SDL.h>
 #include <SDL_image.h>
 
 namespace ktp {
 
-namespace SDL2_Image {
+class SDL2_Image {
+ public:
 
-  void closeImage();
-  bool initImage();
-  void queryImageVersions();
+  static void closeImage() { IMG_Quit(); }
 
-  inline SDL_version image_compiled_version_{};
-  inline const SDL_version* image_linked_version_;
+  static bool initImage() {
+    queryImageVersions();
+    logMessage("SDL_image compiled version: " +
+                std::to_string(image_compiled_version_.major) + '.' +
+                std::to_string(image_compiled_version_.minor) + '.' +
+                std::to_string(image_compiled_version_.patch));
+    logMessage("SDL_image linked version: " +
+                std::to_string(image_linked_version_->major) + '.' +
+                std::to_string(image_linked_version_->minor) + '.' +
+                std::to_string(image_linked_version_->patch));
 
-} // end namespace SDL2_Image
+    // Flags available: IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF
+    constexpr auto flags = IMG_INIT_PNG;
+    const auto initted = IMG_Init(flags);
+    if((initted & flags) != flags) {
+      logSDL2Error("IMG_Init");
+      return false;
+    }
+    return true;
+  }
+
+ private:
+
+  static void queryImageVersions() {
+    SDL_IMAGE_VERSION(&image_compiled_version_);
+    image_linked_version_ = IMG_Linked_Version();
+  }
+
+  inline static       SDL_version image_compiled_version_ {};
+  inline static const SDL_version* image_linked_version_ {nullptr};
+};
 
 } // end namespace ktp
 
