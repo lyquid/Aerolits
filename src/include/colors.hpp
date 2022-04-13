ca5935dc3f4c55f85eab4aa95b91775a82273072
Alex CB <ketepincho@gmail.com>
2022-04-13 18:30:38 +0200

    Fixed warnings in colors

--- src/include/colors.hpp
+++ src/include/colors.hpp
@@ -13,28 +13,10 @@ class Color {
     r(red), g(green), b(blue), a(alpha) {}
 
   Color(int red, int green, int blue, int alpha = 255):
-    r(red * inv), g(green * inv), b(blue * inv), a(alpha * inv) {}
+    r((float)red * kInv), g((float)green * kInv), b((float)blue * kInv), a((float)alpha * kInv) {}
 
   Color(unsigned int red, unsigned int green, unsigned int blue, unsigned int alpha = 255u):
-    r(red * inv), g(green * inv), b(blue * inv), a(alpha * inv) {}
-
-  // template<typename T>
-  // auto operator*(T num) {
-  //   r = r * num;
-  //   g = g * num;
-  //   b = b * num;
-  //   a = a * num;
-  //   return *this;
-  // }
-
-  // template<typename T>
-  // auto operator+(T num) {
-  //   r = r + num;
-  //   g = g + num;
-  //   b = b + num;
-  //   a = a + num;
-  //   return *this;
-  // }
+    r((float)red * kInv), g((float)green * kInv), b((float)blue * kInv), a((float)alpha * kInv) {}
 
   float r, g, b, a;
 
@@ -42,11 +24,11 @@ class Color {
    * @brief Gives you the inverse of 255.
    * @return Something like 0.003921568859f.
    */
-  static auto inv255() { return inv; }
+  static auto inv255() { return kInv; }
 
  private:
 
-  static constexpr float inv {1.f / 255.f};
+  static constexpr float kInv {1.f / 255.f};
 };
 
 } // end namespace ktp
